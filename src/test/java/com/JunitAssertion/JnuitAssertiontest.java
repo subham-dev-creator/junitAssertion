60a699a778ec4184bee9f23ad4739f3ceee7a1af
Madhu Yadav <subhamofficial98@gmail.com>
2020-09-30 00:16:36 +0530

    UC10 Done

--- src/test/java/com/JunitAssertion/JnuitAssertiontest.java
+++ src/test/java/com/JunitAssertion/JnuitAssertiontest.java
@@ -14,4 +14,45 @@ public class JnuitAssertiontest {
         assertEquals(true,b1);
         assertEquals(false,b2);
     }
+
+    @Test
+    public void Testlastname(){
+
+        UserRegistration obj =new UserRegistration();
+        boolean b1 = obj.flNameValidation("Kumar");
+        boolean b2 = obj.flNameValidation("kumar");
+        assertEquals(true,b1);
+        assertEquals(false,b2);
+    }
+
+    @Test
+    public void Testemail(){
+
+        UserRegistration obj =new UserRegistration();
+        boolean b1 = obj.emailValidation("sk@.com");
+        boolean b2 = obj.emailValidation("sk@gmail.com");
+        assertEquals(true,b1);
+        assertEquals(false,b2);
+    }
+
+    @Test
+    public void Testmob(){
+
+        UserRegistration obj =new UserRegistration();
+        boolean b1 = obj.mobNumValidation("918802616474");
+        boolean b2 = obj.mobNumValidation("894949181");
+        assertEquals(true,b1);
+        assertEquals(false,b2);
+    }
+
+    @Test
+    public void Testpass(){
+
+        UserRegistration obj =new UserRegistration();
+        boolean b1 = obj.passValidation("Skpass80@");
+        boolean b2 = obj.passValidation("skpass");
+        assertEquals(true,b1);
+        assertEquals(false,b2);
+    }
+
 }
